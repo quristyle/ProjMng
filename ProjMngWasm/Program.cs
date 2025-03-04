92f11e51555fd620fba0dc33d4a622c56dfaa7d6
이순열 <quristyle@gmail.com>
2025-03-04 13:26:34 +0900

    a

--- ProjMngWasm/Program.cs
+++ ProjMngWasm/Program.cs
@@ -29,8 +29,8 @@ builder.Services.AddHttpClient<IUMSService, UMSService>(client => {
 
 
 builder.Services.AddHttpClient<IJsiniService, JsiniService>(client => {
-  client.BaseAddress = new Uri("http://localhost:5267");
-  //client.BaseAddress = new Uri("https://api.jsini.co.kr");
+  //client.BaseAddress = new Uri("http://localhost:5267");
+  client.BaseAddress = new Uri("https://api.jsini.co.kr");
 });
 
 #else
