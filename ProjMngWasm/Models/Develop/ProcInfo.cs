8fc74dbfd9ee2d1d5f13c47af4b895903f8f6b79
이순열 <quristyle@gmail.com>
2025-03-05 18:19:43 +0900

    main init

--- ProjMngWasm/Models/Develop/ProcInfo.cs
+++ ProjMngWasm/Models/Develop/ProcInfo.cs
@@ -1,4 +1,6 @@
-﻿namespace ProjMngWasm.Models.Develop {
+﻿using Radzen.Blazor;
+
+namespace ProjMngWasm.Models.Develop {
   public class ProcInfo {
     public string ProcedureName { get; set; }
     public string Description { get; set; }
@@ -16,6 +18,10 @@
   public class ColumnInfo {
     public string ColunmName { get; set; }
     public string Description { get; set; }
+    public string Type_NM { get; set; }
+    public string Max_LENGTH { get; set; }
+    public string NullAble { get; set; }
+    public string Ident { get; set; }
 
   }
 
