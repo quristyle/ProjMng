8e264cbaa23b91b28c77a5d15939feda20e1c477
DESKTOP-4FL89DV\q <quristyle@gmail.com>
2025-06-13 17:49:46 +0900

    quriGrid init

---
+++ ProjMngWasm/Compnents/QuriGrid.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using Radzen.Blazor;
+using WasmShear.Services;
+
+namespace ProjMngWasm.Compnents {
+  public class QuriGrid<TItem> : RadzenDataGrid<TItem> {
+
+    [Inject] protected DevService? devService { get; set; }
+    public QuriGrid() {
+      Style = "height:100%;";
+      FilterCaseSensitivity = Radzen. FilterCaseSensitivity.CaseInsensitive;
+      LogicalFilterOperator = Radzen.LogicalFilterOperator.And;
+      AllowFiltering = true;
+      AllowColumnResize = true;
+      FilterMode = Radzen.FilterMode.Simple;
+      SelectionMode = Radzen.DataGridSelectionMode.Single;
+      AllowPaging = false;
+      AllowSorting = true;
+      PagerHorizontalAlign = Radzen.HorizontalAlign.Left;
+
+      ShowPagingSummary = true;
+    }
+
+    //public List<T>? Data { get; set; }
+    public async Task<IEnumerable<TItem>> Load(string procName, Dictionary<string, string> dic) {
+
+      IsLoading = true;
+      await InvokeAsync(StateHasChanged);
+
+      var ri = await devService.GetList<TItem>(procName, dic);
+      Data = ri.Data.ToList();
+
+      IsLoading = false;
+      //await Reload();
+      await InvokeAsync(StateHasChanged);
+      return Data;
+    }
+
+
+  }
+}
