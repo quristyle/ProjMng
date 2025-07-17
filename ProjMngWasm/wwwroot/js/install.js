dbedf449c8c8fcf9030caa692ab2dceb2c601f68
quristyle <quristyle@gmail.com>
2025-07-17 22:15:51 +0900

    .......

--- ProjMngWasm/wwwroot/js/install.js
+++ ProjMngWasm/wwwroot/js/install.js
@@ -23,3 +23,18 @@ function installPWA ( ) {
   deferredInstallPrompt.prompt();
 }
 
+
+
+
+// wwwroot/js/excelDownload.js
+window.downloadExcel = (data, filename) => {
+  const ws = XLSX.utils.aoa_to_sheet(data);  // 데이터 변환
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+
+  // 엑셀 파일 다운로드
+  XLSX.writeFile(wb, filename);
+};
+
+
+
